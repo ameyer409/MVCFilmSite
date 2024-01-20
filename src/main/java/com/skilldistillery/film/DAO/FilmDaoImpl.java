23e351f01a7a1afcb3e92f5f4a180cc8d8ce18ba
Allan Meyer <ameyer409@yahoo.com>
2024-01-20 18:10:05 -0500

    fleshed out update functionality

--- src/main/java/com/skilldistillery/film/DAO/FilmDaoImpl.java
+++ src/main/java/com/skilldistillery/film/DAO/FilmDaoImpl.java
@@ -38,12 +38,20 @@ public class FilmDaoImpl implements DatabaseAccessor {
 		try {
 			conn = DriverManager.getConnection(URL, USER, PWD);
 			conn.setAutoCommit(false);
-			String sql = "UPDATE film SET title = ?, description = ?, release_year = ?\n" + "WHERE id = ?;";
+			String sql = "UPDATE film SET title = ?, description = ?, release_year = ?, language_id = ?, "
+					+ "rental_duration = ?, rental_rate = ?, length = ?, replacement_cost = ?, rating = ?\n"
+					+ "WHERE id = ?;";
 			PreparedStatement stmt = conn.prepareStatement(sql);
 			stmt.setString(1, film.getTitle());
 			stmt.setString(2, film.getDescription());
 			stmt.setInt(3, film.getReleaseYear());
-			stmt.setInt(4, film.getId());
+			stmt.setInt(4, film.getLanguageId());
+			stmt.setInt(5, film.getRentalDuration());
+			stmt.setDouble(6, film.getRentalRate());
+			stmt.setInt(7, film.getLength());
+			stmt.setDouble(8, film.getReplacementCost());
+			stmt.setString(9, film.getRating());
+			stmt.setInt(10, film.getId());
 			int updateCount = stmt.executeUpdate();
 			System.out.println(updateCount);
 			conn.commit();
