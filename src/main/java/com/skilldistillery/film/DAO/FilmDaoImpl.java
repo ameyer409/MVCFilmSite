2937f20575f27032db779f4bc4569634a02b1cf6
Allan Meyer <ameyer409@yahoo.com>
2024-01-19 20:32:57 -0500

    updates

---
+++ src/main/java/com/skilldistillery/film/DAO/FilmDaoImpl.java
@@ -0,0 +1,8 @@
+package com.skilldistillery.film.DAO;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class FilmDaoImpl implements DatabaseAccessor{
+
+}
--- src/main/java/com/skilldistillery/film/controllers/FilmController.java
+++ src/main/java/com/skilldistillery/film/controllers/FilmController.java
@@ -1,5 +1,19 @@
 package com.skilldistillery.film.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.skilldistillery.film.DAO.DatabaseAccessor;
+
+@Controller
 public class FilmController {
 
+	@Autowired
+	private DatabaseAccessor filmDAO;
+	
+	@RequestMapping("")
+	public String home() {
+		return "WEB-INF/views/home.jsp";
+	}
 }
